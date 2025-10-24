4f72c59401a0b1001233db752e0a9e5a0a5e3f31
Juan Antonio <11.juanantonio.102@gmail.com>
2025-10-24 12:04:53 -0600

    DTOs Apartados

--- src/apartados/dto/create-apartado.dto.ts
+++ src/apartados/dto/create-apartado.dto.ts
@@ -1,1 +1,18 @@
-export class CreateApartadoDto {}
+/* eslint-disable prettier/prettier */
+
+import { IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
+
+export class CreateApartadoDto {
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  readonly cantidad: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly usuarioID: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly bebidasID: number;
+}
