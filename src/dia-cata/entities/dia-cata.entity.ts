fc337d1dfdfe88bdfe356ac77f475dc8b08a8063
Juan Antonio <11.juanantonio.102@gmail.com>
2025-10-24 12:00:32 -0600

    Arreglar bug actualizar usuario

--- src/dia-cata/entities/dia-cata.entity.ts
+++ src/dia-cata/entities/dia-cata.entity.ts
@@ -1,30 +1,27 @@
-import { Usuario } from "src/usuario/entities/usuario.entity";
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+/* eslint-disable prettier/prettier */
+import { Usuario } from 'src/usuario/entities/usuario.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class DiaCata {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column()
-    description: string;
+  @Column()
+  description: string;
 
+  @Column()
+  fecha: Date;
 
-    @Column()
-    fecha: Date;
-
-    @Column()
-    capacidad: number;
-    @Column()
-    costo: number;
-    @OneToMany(()=> Usuario, (usuario)=> usuario.diaCata)
-    usuarios: Usuario[];
-    @Column()
-    estado: boolean;
-    
-
-   
+  @Column()
+  capacidad: number;
+  @Column()
+  costo: number;
+  @OneToMany(() => Usuario, (usuario) => usuario.diaCata)
+  usuarios: Usuario[];
+  @Column()
+  estado: boolean;
 }
--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,18 +1,21 @@
-
-import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
-import { DiaCata } from 'src/dia-cata/entities/dia-cata.entity';
+/* eslint-disable prettier/prettier */
+import {
+  IsEmail,
+  IsNumber,
+  IsString,
+} from 'class-validator';
 export class CreateUsuarioDto {
-    @IsString()
-    nome: string;
+  @IsString()
+  nome: string;
 
-    @IsEmail()
-    email: string;
+  @IsEmail()
+  email: string;
 
-    @IsString()
-    telefono: string;
+  @IsString()
+  telefono: string;
 
-    @IsString()
-    genero: string;
-    @IsNumber()
-    Idcata: DiaCata
+  @IsString()
+  genero: string;
+  @IsNumber()
+  Idcata: number;
 }
